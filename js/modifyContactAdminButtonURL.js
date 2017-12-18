ffd4fb77d8983ab6c0df5adae044cc7b808d8c45
Marly <marlycormar@gmail.com>
2017-12-18 15:29:31 -0500

    Append parameter name-value pairs to final url

--- js/modifyContactAdminButtonURL.js
+++ js/modifyContactAdminButtonURL.js
@@ -5,13 +5,12 @@ $('document').ready(function() {
             var button = searchResults[0];
             var username = document.getElementById('username-reference').textContent
             
-            button.href = contactAdminButtonSettings.url
-                + '&gatorlink=' + contactAdminButtonSettings.username 
-                + '&project_id=' + contactAdminButtonSettings.project_id
-                + '&redcap_username=' + contactAdminButtonSettings.username
-                + '&registered_in_redcap=1'
-                + '&first_name=' + contactAdminButtonSettings.user_firstname
-                + '&last_name=' + contactAdminButtonSettings.user_lastname
-                + '&email=' + contactAdminButtonSettings.email
+            var length = contactAdminButtonValuePairs.length;
+            button.href = contactAdminButtonURL;
+
+            for(var i in contactAdminButtonValuePairs){
+                 button.href += '&' + contactAdminButtonValuePairs[i].name;
+                 button.href += '=' + contactAdminButtonValuePairs[i].value;
+            }
         }
 });
