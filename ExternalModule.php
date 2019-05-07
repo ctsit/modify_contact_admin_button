6d95a4abf16cbb00d293bce78fd9fcafee79d584
G. Rezniczek <g.rezniczek@gmail.com>
2019-05-07 21:25:23 +0200

    Implement project-level settings.

--- ExternalModule.php
+++ ExternalModule.php
@@ -7,13 +7,16 @@
 namespace Modify_Contact_Admin_Button\ExternalModule;
 
 use ExternalModules\AbstractExternalModule;
-use ExternalModules\ExternalModules;
-use User;
 
 /**
  * ExternalModule class for Modify Contact Admin Button.
  */
 class ExternalModule extends AbstractExternalModule {
+
+    function redcap_module_configure_button_display($project_id) {
+        // Only super users may configure on the project level.
+        return SUPER_USER == 1 ? true : null;
+    }
 
     /**
      * @inheritdoc
@@ -22,12 +25,31 @@ class ExternalModule extends AbstractExternalModule {
 
         if($project_id) {
 
-            global $user_firstname, $user_lastname, $user_email, $Proj;
-
-            $length = sizeof($this->getSystemSetting('contact-admin-button-parameters-list-key'));
-            $parameter_names = $this->getSystemSetting('contact-admin-button-parameter-name');
-            $parameter_values = $this->getSystemSetting('contact-admin-button-parameter-value');
-            $url = $this->getSystemSetting('contact-admin-button-url-key');
+            global $user_firstname, $user_lastname, $user_email;
+            
+            $url = "";
+            $label = "";
+            $same_tab = false;
+            $hide = false;
+            if($this->getProjectSetting('contact-admin-button-override-project')) {
+                // Use project overrides.
+                $url = $this->getProjectSetting('contact-admin-button-url-key-project');
+                $length = sizeof($this->getProjectSetting('contact-admin-button-parameters-list-key-project'));
+                $parameter_names = $this->getProjectSetting('contact-admin-button-parameter-name-project');
+                $parameter_values = $this->getProjectSetting('contact-admin-button-parameter-value-project');
+                $label = $this->getProjectSetting('contact-admin-button-label-project');
+                $same_tab = $this->getProjectSetting('contact-admin-button-sametab-project');
+                $hide = $this->getProjectSetting('contact-admin-button-hide-project');
+            }
+            if (!strlen($url)) {
+                $url = $this->getSystemSetting('contact-admin-button-url-key');
+                $length = sizeof($this->getSystemSetting('contact-admin-button-parameters-list-key'));
+                $parameter_names = $this->getSystemSetting('contact-admin-button-parameter-name');
+                $parameter_values = $this->getSystemSetting('contact-admin-button-parameter-value');
+            }
+            if (!strlen($label)) {
+                $label = $this->getSystemSetting('contact-admin-button-label');
+            }
 
             $settings = [
                     "user_firstname"=> $user_firstname,
@@ -37,22 +59,24 @@ class ExternalModule extends AbstractExternalModule {
                     "USERID" => USERID,
             ];
 
+            // Build URL.
             for($i = 0; $i < $length; $i++){
                 if ($parameter_names[$i] and $parameter_values[$i]){
-                    $url .= '&' . filter_var($parameter_names[$i],FILTER_SANITIZE_URL) . 
-                    '=' . $settings[$parameter_values[$i]];
+                    $url .= '&' . filter_var($parameter_names[$i], FILTER_SANITIZE_URL) . 
+                            '=' . $settings[$parameter_values[$i]];
                 }
             }
 
-            $this->sendVarToJS('contactAdminButtonURL', $url);
-            
-            // If the user entered a url, change the 'Contact REDCap administrator' button href.
-            if($url){
-                $this->includeJs('js/modifyContactAdminButtonURL.js');
-            }
+            // Send data to JavaScript.
+            $contactAdminButtonSettings = array (
+                "url" => $url,
+                "label" => strlen($label) ? str_ireplace("<script", "&lt;script", $label) : "",
+                "sameTab" => $same_tab,
+                "hide" => $hide,
+            );
+            $this->sendVarToJS('contactAdminButtonSettings', $contactAdminButtonSettings);
+            $this->includeJs('js/modifyContactAdminButtonURL.js');
         }
-        
-        return;
     }
 
     /**
@@ -76,6 +100,4 @@ class ExternalModule extends AbstractExternalModule {
     protected function sendVarToJS($name, $value) {
         echo '<script>var '. $name .' = ' . json_encode($value) . ';</script>';
     }
-
-
 }
--- js/modifyContactAdminButtonURL.js
+++ js/modifyContactAdminButtonURL.js
@@ -1,7 +1,9 @@
 $('document').ready(function() {
-    var $button = $('#help_panel .btn-contact-admin');
-
-    if ($button.length === 1) {
-        $button.attr('href', contactAdminButtonURL).attr('target', '_blank');
+    var button = $('#help_panel .btn-contact-admin')
+    if (button.length === 1) {
+        if (contactAdminButtonSettings.url.length > 0) button.attr('href', contactAdminButtonSettings.url)
+        if (!contactAdminButtonSettings.sameTab) button.attr('target', '_blank')
+        if (contactAdminButtonSettings.label.length > 0) button.html(contactAdminButtonSettings.label)
+        if (contactAdminButtonSettings.hide) button.parent().hide()
     }
-});
+})
